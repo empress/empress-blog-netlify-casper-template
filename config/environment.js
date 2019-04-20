8ff4096d7682bd5659667562e2d5b181b674f51c
Chris Manson <chris@manson.ie>
2019-04-20 12:35:42 +0100

    update empress-blog and empress-blog-casper-template

--- config/environment.js
+++ config/environment.js
@@ -36,6 +36,16 @@ module.exports = function(environment) {
         route: 'page',
         id: 'chris-manson'
       }]
+    },
+
+    'responsive-image': {
+      sourceDir: 'images',
+      destinationDir: 'responsive-images',
+      quality: 80,
+      supportedWidths: [2000, 1000, 600, 300],
+      removeSourceDir: false,
+      justCopy: false,
+      extensions: ['jpg', 'jpeg', 'png', 'gif']
     }
   };
 
